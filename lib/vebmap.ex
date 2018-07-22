12610546323680ed19d6f4679f5e9403bf2d8ed7
2015级喵班 <SchrodingerZhu>
2018-07-22 11:06:53 +0800

    preparations

---
+++ lib/vebmap.ex
@@ -0,0 +1,18 @@
+defmodule Vebmap do
+  @moduledoc """
+  Documentation for Vebmap.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Vebmap.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/vebmap_test.exs
@@ -0,0 +1,8 @@
+defmodule VebmapTest do
+  use ExUnit.Case
+  doctest Vebmap
+
+  test "greets the world" do
+    assert Vebmap.hello() == :world
+  end
+end
